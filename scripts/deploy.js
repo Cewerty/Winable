3d4373f7e807ee733b37096e3530edb47353a620
Cewerty <88933231+Cewerty@users.noreply.github.com>
2025-02-13 17:36:09 +0500

    Added deploy file

---
+++ scripts/deploy.js
@@ -0,0 +1,49 @@
+import hre from 'hardhat';
+
+async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+
+    const TokenFactory = await hre.ethers.getContractFactory("Token", deployer);
+
+    const initialSupply = hre.ethers.parseUnits('1000000', 18);
+
+    const firstToken = await TokenFactory.deploy(initialSupply, "FirstToken", 18, "FT");
+
+    firstToken.waitForDeployment();
+
+    const firstTokenAddress = await firstToken.getAddress();
+
+    const secondToken = await TokenFactory.deploy(initialSupply, "SecondToken", 18, "ST");
+
+    secondToken.waitForDeployment();
+
+    const secondTokenAddress = await secondToken.getAddress();
+
+    const factoryFactory = await hre.ethers.getContractFactory("Factory", deployer);
+
+    const factory = await factoryFactory.deploy();
+
+    factory.waitForDeployment();
+
+    const factoryAddress = await factory.getAddress();
+
+    const routerFactory = await hre.ethers.getContractFactory("Router");
+
+    const router = await routerFactory.deploy(factoryAddress);
+
+    router.waitForDeployment();
+
+    const routerAddress = await router.getAddress();
+
+    console.log("Token A: ", firstTokenAddress);
+    console.log("Token B: ", secondTokenAddress);
+    console.log("Factory: ", factoryAddress);
+    console.log("Router: ", routerAddress);
+}
+
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+    console.error('Ошибка в процессе развертывания:', error);
+    process.exit(1);
+});
